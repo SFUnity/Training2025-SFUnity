212be91177c4031b9423e41845c89a4e8b14ba2b
MINAJECHUNG <mchung27@urbanschool.org>
2025-01-07 21:52:06 -0800

    start work on ElevatorIO and ElevatorIOSparkMax

--- src/main/java/frc/robot/subsystems/reef/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/reef/elevator/ElevatorConstants.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems.reef.elevator;
-
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import frc.robot.constants.Constants;
 public class ElevatorConstants {
-    private final int elevatorMotorID = 0;
-    private final double maxElevatorSpeed = 0;
-    private final double maxElevatorAcceleration = 0;
+    public static final int elevatorMotorID = 0;
+    public static final double maxElevatorSpeed = 0;
+    public static final double maxElevatorAcceleration = 0;
 }
--- src/main/java/frc/robot/subsystems/reef/elevator/ElevatorIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/reef/elevator/ElevatorIOSparkMax.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.reef.elevator;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
@@ -11,13 +12,18 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
+import static frc.robot.subsystems.reef.elevator.ElevatorConstants.*;
+
 public class ElevatorIOSparkMax implements ElevatorIO{
-    private final TrapezoidProfile profile =
-      new TrapezoidProfile(new TrapezoidProfile.Constraints(1.75, 0.75));
-         private TrapezoidProfile.State goal = new TrapezoidProfile.State();
-         private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
-    
-         public ElevatorIOSparkMax() {
+  private final SparkMax elevatorMotor = new SparkMax(elevatorMotorID, MotorType.kBrushless);  
+  
+  private final TrapezoidProfile profile =
+      new TrapezoidProfile(new TrapezoidProfile.Constraints(ElevatorConstants.maxElevatorSpeed, ElevatorConstants.maxElevatorAcceleration));
+    private TrapezoidProfile.State goal = new TrapezoidProfile.State();
+    private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
+    private final RelativeEncoder encoder = elevatorMotor.getEncoder();
+    private final SparkClosedLoopController pid = elevatorMotor.getClosedLoopController();
+    public ElevatorIOSparkMax() {
         
 
     }
