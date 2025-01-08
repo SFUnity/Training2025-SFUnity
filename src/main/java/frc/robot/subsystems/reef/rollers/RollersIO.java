e426ceb5a64e8e1fbbb3e0e8745ce71d8801bcd1
MINAJECHUNG <mchung27@urbanschool.org>
2025-01-08 13:06:19 -0800

    Create Roller IO

--- src/main/java/frc/robot/subsystems/reef/rollers/RollersIO.java
+++ src/main/java/frc/robot/subsystems/reef/rollers/RollersIO.java
@@ -1,3 +1,18 @@
 package frc.robot.subsystems.reef.rollers;
+import org.littletonrobotics.junction.AutoLog;
 
-public interface RollersIO {}
+public interface RollersIO {
+    @AutoLog
+    public static class RollersIOInputs {
+      public double positionRots;
+      public double velocityRotsPerSec = 0.0;
+      public double appliedVolts = 0.0;
+      public double[] currentAmps = new double[] {};
+    }
+  
+    public default void updateInputs(RollersIOInputs inputs) {}
+  
+    public default void runVolts(double volts) {}
+  
+    public default void stop() {}
+  }
