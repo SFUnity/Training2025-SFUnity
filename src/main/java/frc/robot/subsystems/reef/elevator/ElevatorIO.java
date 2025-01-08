2722a713a91fdb0da83a92a7eff815a589a19d5c
MINAJECHUNG <mchung27@urbanschool.org>
2025-01-07 21:52:06 -0800

    start work on ElevatorIO and ElevatorIOSparkMax

--- src/main/java/frc/robot/subsystems/reef/elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/reef/elevator/ElevatorIO.java
@@ -2,7 +2,15 @@ package frc.robot.subsystems.reef.elevator;
 
 import org.littletonrobotics.junction.AutoLog;
 public interface ElevatorIO{
-
+    @AutoLog
+    public static class ElevatorIOInputs{
+        public double positionRots = 0.0;
+        public double velocityRotsPerSec = 0.0;
+        public double appliedVolts = 0.0;
+        public double[] currentAmps = new double[] {};
+    }
+    public default void updateInputs(ElevatorIOInputs inputs){ }
+    
 }
 
 //Calculate
