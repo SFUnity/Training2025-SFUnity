87142bf1447971d15b7169fe22cd21fdfcb09a73
MINAJECHUNG <mchung27@urbanschool.org>
2025-01-07 21:04:34 -0800

    create ElevatorConstants

---
+++ src/main/java/frc/robot/subsystems/reef/elevator/ElevatorConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.reef.elevator;
+
+public class ElevatorConstants {
+    private final int elevatorMotorID = 0;
+    private final double maxElevatorSpeed = 0;
+    private final double maxElevatorAcceleration = 0;
+}
