434a02d70afaee2827abc1f0b119af49290b85b5
MINAJECHUNG <mchung27@urbanschool.org>
2025-01-08 09:39:47 -0800

    format

--- src/main/java/frc/robot/subsystems/reef/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/reef/elevator/Elevator.java
@@ -3,16 +3,14 @@ package frc.robot.subsystems.reef.elevator;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
 public class Elevator {
-    private final TrapezoidProfile profile =
+  private final TrapezoidProfile profile =
       new TrapezoidProfile(
           new TrapezoidProfile.Constraints(
               ElevatorConstants.maxElevatorSpeed, ElevatorConstants.maxElevatorAcceleration));
   private TrapezoidProfile.State goal = new TrapezoidProfile.State();
   private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
 
-  public Elevator(){
-
-  }
+  public Elevator() {}
 
   private void calculateDesiredAngle(double kP) {
     goal = new TrapezoidProfile.State(kP, 0);
