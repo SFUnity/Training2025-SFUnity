e85c829fb8c3dc78c68123597ab77d2e6e39f5ce
MINAJECHUNG <mchung27@urbanschool.org>
2025-01-08 13:33:38 -0800

    preliminary roller.java done

--- src/main/java/frc/robot/subsystems/reef/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/reef/rollers/Rollers.java
@@ -1,3 +1,43 @@
 package frc.robot.subsystems.reef.rollers;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Rollers {}
+public class Rollers extends SubsystemBase{
+    private final RollersIO io;
+    private final RollersIOInputsAutoLogged inputs;
+
+    public Rollers(RollersIO io, RollersIOInputsAutoLogged inputs){
+        this.io = io;
+        this.inputs = inputs;
+    }
+
+    @Override
+    public void periodic(){
+        io.updateInputs(inputs);
+    }
+
+    private void runIntakingRollers(){
+        io.runVolts(RollersConstants.rollersIntakingSpeed);
+    }
+    private void runPlaceRollers(){
+        io.runVolts(RollersConstants.rollersPlaceSpeed);
+    }
+
+
+    private void stopRollers(){
+        io.stop();
+    }
+
+    public Command placeCoral(){
+        return run(() -> {
+            runPlaceRollers();
+        }).withName("placeCoralRollers");
+    }
+
+    public Command intakeCoral(){
+        return run(() -> {
+            runIntakingRollers();
+        }).withName("intakeCoralRollers");
+    }
+    //TODO: add "until when 2m distance sensor gets set up"
+}
--- src/main/java/frc/robot/subsystems/reef/rollers/RollersConstants.java
+++ src/main/java/frc/robot/subsystems/reef/rollers/RollersConstants.java
@@ -2,4 +2,7 @@ package frc.robot.subsystems.reef.rollers;
 
 public class RollersConstants {
   public static final int rollerMotorID = 0;
+
+  public static final double rollersIntakingSpeed = 0;
+  public static final double rollersPlaceSpeed = 0;
 }
