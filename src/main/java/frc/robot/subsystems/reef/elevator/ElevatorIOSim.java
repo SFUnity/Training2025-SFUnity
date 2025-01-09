8e7c2393fd609edb5a2296ea8a68d0d71962e14a
MINAJECHUNG <mchung27@urbanschool.org>
2025-01-09 10:08:06 -0800

    finish elevatorIOSim

--- src/main/java/frc/robot/subsystems/reef/elevator/ElevatorIOSim.java
+++ src/main/java/frc/robot/subsystems/reef/elevator/ElevatorIOSim.java
@@ -1,19 +1,16 @@
 package frc.robot.subsystems.reef.elevator;
 
+import static edu.wpi.first.units.Units.Meters;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.units.measure.Angle;
-import static edu.wpi.first.units.Units.Meters
-;
-public class ElevatorIOSim extends SubsystemBase{
+
+public class ElevatorIOSim implements ElevatorIO {
   private final PIDController pid;
-  private final ElevatorIOInputsAutoLogged inputs;
 
   private double appliedVolts = 0.0;
-  private final ElevatorSim sim = 
+  private final ElevatorSim sim =
       new ElevatorSim(
           DCMotor.getNEO(1),
           1,
@@ -24,17 +21,28 @@ public class ElevatorIOSim extends SubsystemBase{
           true,
           0);
 
-  public ElevatorIOSim(ElevatorIOInputsAutoLogged inputs) {
-    this.inputs = inputs;
+  public ElevatorIOSim() {
     pid = new PIDController(0.0, 0.0, 0.0);
     sim.setState(0.0, 0.0);
   }
 
   @Override
-  public void periodic(){
+  public void updateInputs(ElevatorIOInputs inputs) {
     inputs.position = Meters.of(sim.getPositionMeters());
     inputs.velocityMetersPerSec = sim.getVelocityMetersPerSecond();
     inputs.appliedVolts = appliedVolts;
     inputs.currentAmps = new double[] {sim.getCurrentDrawAmps()};
+
+  }
+
+  @Override
+  public void runVolts(double volts) {
+    appliedVolts = volts;
+    sim.setInputVoltage(volts);
+  }
+
+  @Override
+  public void stop(){
+    runVolts(0);
   }
 }
