b9dfc0126c2fd570f6ee192d6f3bc037f3109c6b
MrTinker64 <avi.micah@gmail.com>
2025-01-11 08:57:53 -0800

    format

--- src/main/java/frc/robot/subsystems/reef/elevator/ElevatorVisualizer.java
+++ src/main/java/frc/robot/subsystems/reef/elevator/ElevatorVisualizer.java
@@ -1,15 +1,12 @@
 package frc.robot.subsystems.reef.elevator;
 
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
-import frc.robot.util.LoggedTunableNumber;
 import org.littletonrobotics.junction.Logger;
 
 public class ElevatorVisualizer {
