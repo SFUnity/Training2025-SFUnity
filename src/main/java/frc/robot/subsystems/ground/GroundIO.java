429f7b04c08fdf095282318cbd23dbb249e526e8
Kyle Lin <klin28@urbanschool.org>
2025-01-12 14:39:59 -0800

    Add basic methods and volts/ampage of the pivots and rollers to groundIO

--- src/main/java/frc/robot/subsystems/ground/GroundIO.java
+++ src/main/java/frc/robot/subsystems/ground/GroundIO.java
@@ -1,3 +1,21 @@
 package frc.robot.subsystems.ground;
 
-public interface GroundIO {}
+import org.littletonrobotics.junction.AutoLog;
+
+public interface GroundIO {
+    @AutoLog
+    public static class GroundIOInputs {
+        public double pivotAppliedVolts = 0.0;
+        public double pivotCurrentAmps = 0.0;
+
+        public double rollersAppliedVolts = 0.0;
+        public double rollersCurrentAmps = 0.0;
+
+    }
+
+    default void updateInputs (GroundIOInputs inputs) {}
+
+    default void setPivotPosition () {}
+
+    default void stop() {}
+}
