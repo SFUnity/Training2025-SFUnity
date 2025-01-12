fd5be83870bd6fcec5a9f353c5bfd379173de588
MrTinker64 <avi.micah@gmail.com>
2025-01-12 12:58:03 -0800

    remove unused

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -13,7 +13,6 @@
 
 package frc.robot.subsystems.drive;
 
-import static edu.wpi.first.units.Units.*;
 import static frc.robot.subsystems.drive.DriveConstants.*;
 
 import choreo.trajectory.SwerveSample;
