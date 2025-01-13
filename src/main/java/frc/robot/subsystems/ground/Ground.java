ff185a038968b4789255137f506fc7c53a0fd530
Kyle Lin <klin28@urbanschool.org>
2025-01-12 16:33:27 -0800

    Implement GroundIO into Ground.java (unfinished)

--- src/main/java/frc/robot/subsystems/ground/Ground.java
+++ src/main/java/frc/robot/subsystems/ground/Ground.java
@@ -1,3 +1,10 @@
 package frc.robot.subsystems.ground;
 
-public class Ground {}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Ground extends SubsystemBase {
+    private final GroundIO io;
+    public Ground(GroundIO io){
+        this.io = io;
+    }
+}
