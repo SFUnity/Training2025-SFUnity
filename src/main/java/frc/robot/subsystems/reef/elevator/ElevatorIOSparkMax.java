ce6437ae8f700e2093809183193cb9c55752f866
MINAJECHUNG <mchung27@urbanschool.org>
2025-01-14 09:43:35 -0800

    run build

--- src/main/java/frc/robot/subsystems/reef/elevator/ElevatorIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/reef/elevator/ElevatorIOSparkMax.java
@@ -2,11 +2,12 @@ package frc.robot.subsystems.reef.elevator;
 
 import static edu.wpi.first.units.Units.Meters;
 import static frc.robot.subsystems.reef.elevator.ElevatorConstants.*;
-import edu.wpi.first.units.measure.Distance;
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.units.*;
+import edu.wpi.first.units.measure.Distance;
 
 public class ElevatorIOSparkMax implements ElevatorIO {
   private final SparkMax elevatorMotor = new SparkMax(elevatorMotorID, MotorType.kBrushless);
@@ -17,6 +18,7 @@ public class ElevatorIOSparkMax implements ElevatorIO {
   private long currentTime;
   private double deltaPosition = 0;
   private double deltaTime = 0;
+
   public ElevatorIOSparkMax() {}
 
   @Override
@@ -27,9 +29,9 @@ public class ElevatorIOSparkMax implements ElevatorIO {
     prevoiusPosition = inputs.position;
     inputs.position = Meters.of(encoder.getPosition());
     deltaPosition = inputs.position.in(Meters) - prevoiusPosition.in(Meters);
-    deltaTime = (currentTime - prevoiusTime)/1e9;
-    inputs.velocityMetersPerSec = deltaPosition/deltaTime;
-    
+    deltaTime = (currentTime - prevoiusTime) / 1e9;
+    inputs.velocityMetersPerSec = deltaPosition / deltaTime;
+
     inputs.appliedVolts = elevatorMotor.getAppliedOutput() * elevatorMotor.getBusVoltage();
     inputs.currentAmps =
         new double[] {elevatorMotor.getOutputCurrent(), elevatorMotor.getOutputCurrent()};
