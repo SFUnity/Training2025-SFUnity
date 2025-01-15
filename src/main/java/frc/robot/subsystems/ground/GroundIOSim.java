ded515d6cae56bbcbbe9a790eebca9bc5faa7dc1
Kyle Lin <klin28@urbanschool.org>
2025-01-14 18:37:28 -0800

    Add a SingleJointedArmSim to GroundIOSim

--- src/main/java/frc/robot/subsystems/ground/GroundIOSim.java
+++ src/main/java/frc/robot/subsystems/ground/GroundIOSim.java
@@ -1,3 +1,21 @@
 package frc.robot.subsystems.ground;
 
-public class GroundIOSim {}
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
+public class GroundIOSim implements GroundIO {
+        
+    private final SingleJointedArmSim sim =
+        new SingleJointedArmSim(
+            DCMotor.getNEO(1), 
+            0, 
+            0, 
+            0, 
+            0, 
+            0, 
+            false, 
+            0, 
+            null);
+}
+ 
