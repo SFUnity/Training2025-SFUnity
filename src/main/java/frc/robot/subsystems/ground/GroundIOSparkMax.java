c24073fcdc477c3a493d9e129f3eb7106d72882a
Lucas Lee <llee28@urbanschool.org>
2025-01-18 13:44:37 -0800

    Create PWMSparkMax variables to GroundIOSparkMax

--- src/main/java/frc/robot/subsystems/ground/GroundIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/ground/GroundIOSparkMax.java
@@ -1,3 +1,12 @@
 package frc.robot.subsystems.ground;
 
-public class GroundIOSparkMax implements GroundIO {}
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class GroundIOSparkMax implements GroundIO {
+    private final PWMSparkMax leader = new PWMSparkMax();
+    private final PWMSparkMax follower = new PWMSparkMax();
+    private final RelativeEncoder encoder = leader.getEncoder();
+    private final SparkClosedLoopController pid = leader.getPIDController();
+}
