a56c50389b8b999c49d618eeb37ffccf506ec5bd
Kyle Lin <klin28@urbanschool.org>
2025-01-18 13:56:41 -0800

    Import necessary files to GroundIOSparkMax

--- src/main/java/frc/robot/subsystems/ground/GroundIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/ground/GroundIOSparkMax.java
@@ -4,9 +4,11 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
 public class GroundIOSparkMax implements GroundIO {
-    private final PWMSparkMax leader = new PWMSparkMax();
-    private final PWMSparkMax follower = new PWMSparkMax();
+    private final PWMSparkMax leader = new PWMSparkMax(0);
+    private final PWMSparkMax follower = new PWMSparkMax(0);
     private final RelativeEncoder encoder = leader.getEncoder();
     private final SparkClosedLoopController pid = leader.getPIDController();
 }
