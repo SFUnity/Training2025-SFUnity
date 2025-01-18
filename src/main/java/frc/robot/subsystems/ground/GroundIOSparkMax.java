1380a2bf99bba8043c49c32afc4d4b98d5e35ff4
Lucas Lee <llee28@urbanschool.org>
2025-01-18 14:43:22 -0800

    Switched from PWMSparkMax to SparkMax. Update variable names to fit ground intake

--- src/main/java/frc/robot/subsystems/ground/GroundIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/ground/GroundIOSparkMax.java
@@ -1,14 +1,17 @@
 package frc.robot.subsystems.ground;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class GroundIOSparkMax implements GroundIO {
-    private final PWMSparkMax leader = new PWMSparkMax(0);
-    private final PWMSparkMax follower = new PWMSparkMax(0);
-    private final RelativeEncoder encoder = leader.getEncoder();
-    private final SparkClosedLoopController pid = leader.getPIDController();
+    private final SparkMax pivot = new SparkMax(0, null);
+    private final SparkMax rollers = new SparkMax(0, null);
+    private final RelativeEncoder encoder = pivot.getEncoder();
+    private final SparkClosedLoopController pid = pivot.getClosedLoopController();
 }
