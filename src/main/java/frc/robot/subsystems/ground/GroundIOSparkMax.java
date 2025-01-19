6a64d6d02f4efb8d38d0927f93864e45220d24a2
Lucas Lee <llee28@urbanschool.org>
2025-01-18 16:43:29 -0800

    Add overrides for stop, updateInputs, runGroundRollers, and setPivotPosition

--- src/main/java/frc/robot/subsystems/ground/GroundIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/ground/GroundIOSparkMax.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.ground;
 
+import com.ctre.phoenix6.controls.Follower;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkClosedLoopController;
@@ -7,11 +8,43 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class GroundIOSparkMax implements GroundIO {
+    private static final double GEAR_RATIO = 1;
     private final SparkMax pivot = new SparkMax(0, null);
     private final SparkMax rollers = new SparkMax(0, null);
     private final RelativeEncoder encoder = pivot.getEncoder();
     private final SparkClosedLoopController pid = pivot.getClosedLoopController();
+
+    @Override
+    public void stop() {
+        pivot.stopMotor();
+    }
+
+    @Override
+    public void updateInputs(GroundIOInputs inputs) {
+        inputs.pivotCurrentPosition = Units.rotationsToDegrees(encoder.getPosition() / GEAR_RATIO);
+        inputs.pivotAppliedVolts = pivot.getAppliedOutput() * pivot.getBusVoltage();
+        inputs.pivotCurrentAmps =  pivot.getOutputCurrent();
+
+        inputs.rollersAppliedVolts = rollers.getAppliedOutput() * rollers.getBusVoltage();
+        inputs.rollersCurrentAmps = rollers.getOutputCurrent();
+
+    }
+
+    @Override
+    public void runGroundRollers(double percentOutput) {
+        rollers.set(percentOutput); 
+    }
+
+    @Override
+    public void setPivotPosition(double setpointRots) {
+        if (controllerNeedsReset) {
+            controller.reset();
+            controllerNeedsReset = false;
+        }
+    }
+
 }
