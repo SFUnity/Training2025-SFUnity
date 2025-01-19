0657cdaa1fc04e71422b87284f4cfea4cefb3b69
David Peteres <Dlpeters1111@gmail.com>
2025-01-19 13:50:26 -0800

    auto paths

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -7,11 +7,8 @@ import choreo.auto.AutoTrajectory;
 import choreo.trajectory.SwerveSample;
 import choreo.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.util.AllianceFlipUtil;
 import frc.robot.util.PoseManager;
@@ -52,21 +49,19 @@ public class Autos {
                   traj.getInitialPose(AllianceFlipUtil.shouldFlip()).get());
             });
 
-
-    //TODO:
-    //factory
-    //.bind("scoreCoral", SCORECMD())
-    //.bind("RemoveAlgae", RemoveAlageCMD())
+    // TODO:
+    // factory
+    // .bind("scoreCoral", SCORECMD())
+    // .bind("RemoveAlgae", RemoveAlageCMD())
 
     chooser = new AutoChooser();
-        // autoChooser.addCmd("Example Auto Command", this::exampleAutoCommand);
+    // autoChooser.addCmd("Example Auto Command", this::exampleAutoCommand);
 
-        // Put the auto chooser on the dashboard
-       // SmartDashboard.putData(autoChooser);
+    // Put the auto chooser on the dashboard
+    // SmartDashboard.putData(autoChooser);
 
-        // Schedule the selected auto during the autonomous period
-        //RobotModeTriggers.autonomous().whileTrue(autoChooser.selectedCommandScheduler());
-    
+    // Schedule the selected auto during the autonomous period
+    // RobotModeTriggers.autonomous().whileTrue(autoChooser.selectedCommandScheduler());
 
     if (!DriverStation.isFMSAttached()) {
       // Set up test choreo routines
@@ -94,78 +89,77 @@ public class Autos {
     return isChoreoAuto ? chooser.selectedCommandScheduler() : nonChoreoChooser.get();
   }
 
-
   // Routines
-public Command CenterWallLKAlgaeL1() {
+  public Command CenterWallLKAlgaeL1() {
     return Commands.sequence(
-        factory.resetOdometry("CenterWallToLK"), //   
+        factory.resetOdometry("CenterWallToLK"), //
         // PICK UP ALGAE AND SCORE CORAL
         factory.trajectoryCmd("KLEject"), // GET RID OF ALGAE
         factory.trajectoryCmd("KLtoStationHigh"), // GO TO CORAL PICKUP
-        //PICK UP CORAL
-         factory.trajectoryCmd("StationHighToKLL2") // GO TO CORAL
-         //PLACE CORAL
-    );
-}
+        // PICK UP CORAL
+        factory.trajectoryCmd("StationHighToKLL2") // GO TO CORAL
+        // PLACE CORAL
+        );
+  }
 
+  public Command score() {
+    return Commands.sequence(Commands.print("scoring"));
+  }
 
+  public Command intake() {
+    return Commands.sequence(Commands.print("intake coral"));
+  }
 
-public Command score(){
-  return Commands.sequence(
-    Commands.print("scoring")
-  );
-}
-public Command intake(){
-  return Commands.sequence(
-    Commands.print("intake coral")
-  );
-}
-public Command RemoveAlgae(){
-  return Commands.sequence(
-    Commands.print("removing Algae")
-  );
-}
+  public Command RemoveAlgae() {
+    return Commands.sequence(Commands.print("removing Algae"));
+  }
 
- // Options: .done() = when routine is done, AtTime("x") = run cmd on eventMarker, 
-  //.active().whileTrue() =  Trigger while the trajectory is still running.
+  // Options: .done() = when routine is done, AtTime("x") = run cmd on eventMarker,
+  // .active().whileTrue() =  Trigger while the trajectory is still running.
 
-  //Triggers can be combined using logical conditions for branching autos:
-public AutoRoutine SpamLsAuto() {
+  // Triggers can be combined using logical conditions for branching autos:
+  public AutoRoutine SpamLsAuto() {
 
-  AutoRoutine routine = factory.newRoutine("CenterWallLKAlgaeL1");
+    AutoRoutine routine = factory.newRoutine("CenterWallLKAlgaeL1");
 
-  // Load the routine's trajectories
-  AutoTrajectory centerToLK = routine.trajectory("CenterWallToLK");
-  AutoTrajectory lKToStationHigh = routine.trajectory("KLtoStationHigh");
-  AutoTrajectory stationHighToLKL2 = routine.trajectory("StationHighToKLL1");
+    // Load the routine's trajectories
+    AutoTrajectory centerToLK = routine.trajectory("CenterWallToLK");
+    AutoTrajectory lKToStationHigh = routine.trajectory("KLtoStationHigh");
+    AutoTrajectory stationHighToLKL2 = routine.trajectory("StationHighToKLL1");
 
-    // When the routine begins, reset odometry and start the first trajectory 
-      routine.active().onTrue(
-        Commands.sequence(
-            centerToLK.cmd().beforeStarting(Commands.print("moving to LK")),
-            centerToLK.resetOdometry(),
-            centerToLK.cmd()   // start traj
-        )
-    );
-    
-    centerToLK.done().onTrue(                                                   // WHEN WE FINISH LAST PATH
-    Commands.sequence(                                                          // RUN THESE COMMANDS IN SEQUENTIAL ORDER
-        RemoveAlgae().beforeStarting(Commands.print("removing Algae")), // BEFORE REMOVING THE ALGAE PRINT MSG
-        RemoveAlgae(),                                                          // RUN REMOVE ALGAE CMD
-        score().beforeStarting(Commands.print("scoring")),              // BEFORE SCORING CORAL PRINT MSG
-        score(),                                                                // SCORE CORAL
-        lKToStationHigh.cmd()                                                   // START NEXT PATH
-     )
-);
-    lKToStationHigh.done().onTrue(
-    Commands.sequence(
-      intake(),
-      stationHighToLKL2.cmd().beforeStarting(Commands.print("Arrived at Station High, moving to LK")),
-      stationHighToLKL2.cmd()
-  )
-);
+    // When the routine begins, reset odometry and start the first trajectory
+    routine
+        .active()
+        .onTrue(
+            Commands.sequence(
+                centerToLK.cmd().beforeStarting(Commands.print("moving to LK")),
+                centerToLK.resetOdometry(),
+                centerToLK.cmd() // start traj
+                ));
+
+    centerToLK
+        .done()
+        .onTrue( // WHEN WE FINISH LAST PATH
+            Commands.sequence( // RUN THESE COMMANDS IN SEQUENTIAL ORDER
+                RemoveAlgae()
+                    .beforeStarting(
+                        Commands.print("removing Algae")), // BEFORE REMOVING THE ALGAE PRINT MSG
+                RemoveAlgae(), // RUN REMOVE ALGAE CMD
+                score().beforeStarting(Commands.print("scoring")), // BEFORE SCORING CORAL PRINT MSG
+                score(), // SCORE CORAL
+                lKToStationHigh.cmd() // START NEXT PATH
+                ));
+    lKToStationHigh
+        .done()
+        .onTrue(
+            Commands.sequence(
+                intake(),
+                stationHighToLKL2
+                    .cmd()
+                    .beforeStarting(Commands.print("Arrived at Station High, moving to LK")),
+                stationHighToLKL2.cmd()));
     return routine;
-}
+  }
 
   // Commands
 }
