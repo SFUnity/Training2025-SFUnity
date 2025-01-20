80f8cba43dd8165e72cf3d8fa3697c8523bdbc3d
Lucas Lee <llee28@urbanschool.org>
2025-01-19 16:23:57 -0800

    Change dashboardKey

--- src/main/java/frc/robot/subsystems/ground/GroundConstants.java
+++ src/main/java/frc/robot/subsystems/ground/GroundConstants.java
@@ -10,10 +10,10 @@ public class GroundConstants {
   static {
     switch (Constants.currentMode) {
       default:
-        kP = new LoggedTunableNumber("Ground/Tunables/kP", 2.0);
+        kP = new LoggedTunableNumber("Ground/kP", 2.0);
         break;
       case SIM:
-        kP = new LoggedTunableNumber("Ground/SimTunables/kP", 8.0);
+        kP = new LoggedTunableNumber("Ground/simkP", 8.0);
         break;
     }
   }
