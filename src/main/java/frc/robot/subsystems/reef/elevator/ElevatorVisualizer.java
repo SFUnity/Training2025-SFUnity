8876521d39845eb7071b082b7d5bb25931878520
MrTinker64 <avi.micah@gmail.com>
2025-01-20 18:16:29 -0800

    update visualizer stuff

--- src/main/java/frc/robot/subsystems/reef/elevator/ElevatorVisualizer.java
+++ src/main/java/frc/robot/subsystems/reef/elevator/ElevatorVisualizer.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems.reef.elevator;
 
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import org.littletonrobotics.junction.Logger;
@@ -10,32 +12,37 @@ import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
 import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
 
 public class ElevatorVisualizer {
+  private final double ELEVATOR_ANGLE_DEG = 78;
+
   private final LoggedMechanism2d mechanism;
-  private final LoggedMechanismLigament2d elevator;
+  private final LoggedMechanismRoot2d root;
+  private final LoggedMechanismLigament2d carriage;
   private final String key;
 
   public ElevatorVisualizer(String key) {
     this.key = key;
     mechanism = new LoggedMechanism2d(3.0, 3.0, new Color8Bit(Color.kBlack));
-    LoggedMechanismRoot2d root = mechanism.getRoot("elevator", 1.5, 2.0);
-    elevator = new LoggedMechanismLigament2d("elevator", 0, 0);
+    root = mechanism.getRoot("Elevator", 0, Units.inchesToMeters(1.1));
+    carriage = new LoggedMechanismLigament2d("Carriage", 0, ELEVATOR_ANGLE_DEG);
 
-    root.append(elevator);
+    root.append(carriage);
   }
 
   /** Update intake visualizer with current intake angle */
   public void update(double heightMeters) {
     // Log Mechanism2d
-    elevator.setLength(heightMeters);
+    carriage.setLength(heightMeters);
     Logger.recordOutput("Elevator/Mechanism2d/" + key, mechanism);
 
     // Log 3D poses
-    Pose3d elevator =
+    Rotation2d elevatorAngle = Rotation2d.fromDegrees(ELEVATOR_ANGLE_DEG);
+    // The inches to meters stuff here will have to be changed
+    Pose3d carriagePose =
         new Pose3d(
-            ElevatorConstants.elevatorOrigin.getX(),
-            heightMeters * 10,
-            ElevatorConstants.elevatorOrigin.getY(),
-            new Rotation3d(0, 0, 0.0));
-    Logger.recordOutput("Elevator/Mechanism3d/" + key, elevator);
+            Units.inchesToMeters(4.5) + carriage.getLength() * elevatorAngle.getCos(),
+            0.0,
+            Units.inchesToMeters(7.0) + carriage.getLength() * elevatorAngle.getSin(),
+            new Rotation3d());
+    Logger.recordOutput("Elevator/Mechanism3d/" + key, carriagePose);
   }
 }
