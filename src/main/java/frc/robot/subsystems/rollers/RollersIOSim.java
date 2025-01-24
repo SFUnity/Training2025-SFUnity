c2fc1530512fa9dba13870349d9c23ff33640ae4
MrTinker64 <avi.micah@gmail.com>
2025-01-24 10:30:17 -0800

    add sim implementation

--- src/main/java/frc/robot/subsystems/rollers/RollersIOSim.java
+++ src/main/java/frc/robot/subsystems/rollers/RollersIOSim.java
@@ -1,5 +1,17 @@
 package frc.robot.subsystems.rollers;
 
 public class RollersIOSim implements RollersIO {
+  private double appliedVolts = 0.0;
+
   public RollersIOSim() {}
+
+  @Override
+  public void updateInputs(RollersIOInputs inputs) {
+    inputs.appliedVolts = appliedVolts;
+  }
+
+  @Override
+  public void runVolts(double volts) {
+    appliedVolts = volts;
+  }
 }
