fecadb67e0bedbaf1b8add24f3fcab4539a21a28
MrTinker64 <avi.micah@gmail.com>
2025-01-23 19:57:58 -0800

    fix more broken constants

--- src/main/java/frc/robot/constantsGlobal/FieldConstants.java
+++ src/main/java/frc/robot/constantsGlobal/FieldConstants.java
@@ -116,27 +116,27 @@ public final class FieldConstants {
         Branch.A,
         Branch.B),
     Two(
-        new Pose2d(reefCenter, Rotation2d.fromDegrees(180 - (60 * 1)))
+        new Pose2d(reefCenter, Rotation2d.fromDegrees(180 + (60 * 1)))
             .transformBy(new Transform2d(xOffset, 0, new Rotation2d(Math.PI))),
         Branch.C,
         Branch.D),
     Three(
-        new Pose2d(reefCenter, Rotation2d.fromDegrees(180 - (60 * 2)))
+        new Pose2d(reefCenter, Rotation2d.fromDegrees(180 + (60 * 2)))
             .transformBy(new Transform2d(xOffset, 0, new Rotation2d(Math.PI))),
         Branch.E,
         Branch.F),
     Four(
-        new Pose2d(reefCenter, Rotation2d.fromDegrees(180 - (60 * 3)))
+        new Pose2d(reefCenter, Rotation2d.fromDegrees(180 + (60 * 3)))
             .transformBy(new Transform2d(xOffset, 0, new Rotation2d(Math.PI))),
         Branch.G,
         Branch.H),
     Five(
-        new Pose2d(reefCenter, Rotation2d.fromDegrees(180 - (60 * 4)))
+        new Pose2d(reefCenter, Rotation2d.fromDegrees(180 + (60 * 4)))
             .transformBy(new Transform2d(xOffset, 0, new Rotation2d(Math.PI))),
         Branch.I,
         Branch.J),
     Six(
-        new Pose2d(reefCenter, Rotation2d.fromDegrees(180 - (60 * 5)))
+        new Pose2d(reefCenter, Rotation2d.fromDegrees(180 + (60 * 5)))
             .transformBy(new Transform2d(xOffset, 0, new Rotation2d(Math.PI))),
         Branch.K,
         Branch.L);
