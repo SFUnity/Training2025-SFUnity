ff7fb1d722e6fd73ce505f0118e9b04207a9ca29
MrTinker64 <avi.micah@gmail.com>
2025-01-25 16:59:13 -0800

    add dealgify command

--- src/main/java/frc/robot/RobotCommands.java
+++ src/main/java/frc/robot/RobotCommands.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import static frc.robot.subsystems.elevator.ElevatorConstants.ElevatorHeight.AlgaeHigh;
+import static frc.robot.subsystems.elevator.ElevatorConstants.ElevatorHeight.AlgaeLow;
 import static frc.robot.subsystems.elevator.ElevatorConstants.ElevatorHeight.Source;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -24,5 +26,13 @@ public final class RobotCommands {
         .andThen(carriage.placeCoral())
         .andThen(elevator.disableElevator())
         .withName("score");
+  }
+
+  public static Command dealgify(Elevator elevator, Carriage carriage, boolean high) {
+    return elevator
+        .request(high ? AlgaeHigh : AlgaeLow)
+        .andThen(elevator.enableElevator().until(elevator::atDesiredHeight))
+        .alongWith(high ? carriage.highDelagifiy() : carriage.lowDealgaefy())
+        .withName("dealgify");
   }
 }
