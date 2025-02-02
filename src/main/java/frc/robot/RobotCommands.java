15ec03cfe28ee7a0a19c6a2ba1c064078e7939dd
MINAJECHUNG <mchung27@urbanschool.org>
2025-02-01 16:38:22 -0800

    add back import static of commands

--- src/main/java/frc/robot/RobotCommands.java
+++ src/main/java/frc/robot/RobotCommands.java
@@ -5,12 +5,12 @@ import static frc.robot.RobotCommands.ScoreState.*;
 import static frc.robot.constantsGlobal.FieldConstants.*;
 import static frc.robot.subsystems.elevator.ElevatorConstants.ElevatorHeight.*;
 import static frc.robot.util.AllianceFlipUtil.apply;
+import static edu.wpi.first.wpilibj2.command.Commands.*;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.carriage.Carriage;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.elevator.Elevator;
@@ -27,7 +27,7 @@ public final class RobotCommands {
   public static boolean dealgifyAfterPlacing = false;
 
   public static Command scoreCoral(Elevator elevator, Carriage carriage, PoseManager poseManager) {
-    return Commands.waitUntil(
+    return waitUntil(
             () ->
                 poseManager.getDistanceTo(goalPose(poseManager).get())
                     < elevatorSafeExtensionDistanceMeters.get())
@@ -36,26 +36,26 @@ public final class RobotCommands {
 
   public static Command dealgify(Elevator elevator, Carriage carriage, PoseManager poseManager) {
     BooleanSupplier highAlgae = () -> poseManager.closestFace().highAlgae;
-    return Commands.waitUntil(
+    return waitUntil(
             () ->
                 poseManager.getDistanceTo(goalPose(poseManager).get())
                     < elevatorSafeExtensionDistanceMeters.get())
         .andThen(
-            Commands.either(elevator.request(AlgaeHigh), elevator.request(AlgaeLow), highAlgae)
+            either(elevator.request(AlgaeHigh), elevator.request(AlgaeLow), highAlgae)
                 .andThen(elevator.enableElevator())
                 .alongWith(
-                    Commands.either(carriage.highDealgify(), carriage.lowDealgify(), highAlgae)))
+                    either(carriage.highDealgify(), carriage.lowDealgify(), highAlgae)))
         .alongWith(
-            Commands.runOnce(() -> Logger.recordOutput("HighAlgae", highAlgae.getAsBoolean())));
+            runOnce(() -> Logger.recordOutput("HighAlgae", highAlgae.getAsBoolean())));
   }
 
   public static Command scoreProcessor(
       Carriage carriage, Intake intake, PoseManager poseManager, boolean front) {
-    return Commands.waitUntil(
+    return waitUntil(
             () ->
                 poseManager.getDistanceTo(goalPose(poseManager).get())
                     < processorScoreDistanceMeters.get())
-        .andThen(Commands.either(carriage.scoreProcessor(), intake.poopCmd(), () -> front));
+        .andThen(either(carriage.scoreProcessor(), intake.poopCmd(), () -> front));
   }
 
   public static Supplier<Pose2d> goalPose(PoseManager poseManager) {
@@ -92,7 +92,7 @@ public final class RobotCommands {
 
   public static Command fullIntake(
       Drive drive, Carriage carriage, Intake intake, PoseManager poseManager) {
-    return Commands.select(
+    return select(
         Map.of(
             Source,
                 drive
