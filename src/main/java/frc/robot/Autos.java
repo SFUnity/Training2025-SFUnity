e2b7e92a4dcf2ff48041cbba9f8cdb10b5522919
MrTinker64 <avi.micah@gmail.com>
2025-02-04 17:57:04 -0800

    put auto back

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -188,8 +188,13 @@ public class Autos {
             elevator
                 .request(L1)
                 .andThen(
+                    scoreCoral(
+                        elevator,
+                        carriage,
+                        poseManager,
+                        () -> CenterWallToLKAlgae.getFinalPose().get(),
+                        CenterWallToLKAlgae.done()),
                     runOnce(() -> scoreState = Dealgify),
-                    scoreCoral(elevator, carriage, poseManager, null),
                     dealgify(elevator, carriage, poseManager)));
     CenterWallToLKAlgae.done()
         .onTrue(
@@ -203,13 +208,20 @@ public class Autos {
         .onTrue( // may need to add a small wait command here depending on how mechanical works
             either(StationHighToL.cmd(), StationHighToK.cmd(), () -> coralOnL2 >= 1));
     // For intaking coral see robot.configureBindings, state-based triggers, all the time
-    StationHighToK.done()
+    StationHighToK.active()
+        .and(carriage::coralHeld)
         .onTrue(
             either(
                     elevator.request(L2).finallyDo(() -> coralOnL2 += 1),
                     elevator.request(L3).finallyDo(() -> coralOnL3 += 1),
                     () -> coralOnL3 >= 1)
-                .andThen(scoreCoral(elevator, carriage, poseManager, null)));
+                .andThen(
+                    scoreCoral(
+                        elevator,
+                        carriage,
+                        poseManager,
+                        () -> StationHighToK.getFinalPose().get(),
+                        StationHighToK.done())));
     StationHighToK.done()
         .onTrue(waitUntil(() -> !carriage.coralHeld()).andThen(KToStationHigh.cmd()));
     // StationHighToL.done().onTrue(getAutonomousCommand());
