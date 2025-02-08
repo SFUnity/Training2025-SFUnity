74128d654c52aceb38d6502b06077b8b6e02eab4
MrTinker64 <avi.micah@gmail.com>
2025-02-08 14:44:47 -0800

    put in unit correct pid settings for intake

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -9,14 +9,14 @@ public class IntakeConstants {
 
   public static final boolean pivotInverted = false;
   public static final boolean rollersInverted = false;
-  public static final double pivotPositionFactor = 25;
-  public static final double rollersPositionFactor = 5;
+  public static final double pivotPositionFactor = 25 * 360; // 25 rotations of the motor is 360 degrees of the arm
+  public static final double rollersPositionFactor = 5; // 5 rotations of the motor is 1 rotation of the rollers
 
   public static final double minAngleRads = Units.degreesToRadians(18.39);
   public static final double maxAngleRads = Units.degreesToRadians(87.39);
   public static final double armLengthMeters = Units.inchesToMeters(15);
   public static final double intakePIDMinInput = 0;
-  public static final double intakePIDMaxInput = 2 * Math.PI;
+  public static final double intakePIDMaxInput = 1 * 360;
 
   public static final LoggedTunableNumber algaeVelocityThreshold =
       new LoggedTunableNumber("Intake/algaeVelocityThreshold");
