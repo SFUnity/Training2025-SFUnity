98f386ffbcf5956c118fb55abbbb70344f8d9201
MrTinker64 <avi.micah@gmail.com>
2025-02-16 14:33:21 -0800

    start making list of IOs work properly

--- src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVision.java
+++ src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVision.java
@@ -17,12 +17,19 @@ import org.littletonrobotics.junction.Logger;
 
 public class AprilTagVision extends VirtualSubsystem {
   private final AprilTagVisionIO[] io;
-  private final AprilTagVisionIOInputsAutoLogged inputs = new AprilTagVisionIOInputsAutoLogged();
+  private final AprilTagVisionIOInputsAutoLogged[] inputs;
   private final PoseManager poseManager;
 
   public AprilTagVision(PoseManager poseManager, AprilTagVisionIO... io) {
-    this.io = io;
     this.poseManager = poseManager;
+    this.io = io;
+
+    // Initialize inputs
+    this.inputs = new AprilTagVisionIOInputsAutoLogged[io.length];
+    for (int i = 0; i < inputs.length; i++) {
+      inputs[i] = new AprilTagVisionIOInputsAutoLogged();
+    }
+
     // Pipelines are the setting configured in the limelight software
     for (int i = 0; i < this.io.length; i++) {
       this.io[i].setPipeline(Pipelines.HUMAN_MADE);
@@ -30,30 +37,38 @@ public class AprilTagVision extends VirtualSubsystem {
   }
 
   public void periodic() {
-    for (int i = 0; i < this.io.length; i++) {
-      io[i].updateInputs(inputs, poseManager);
-      Logger.processInputs("AprilTagVision", inputs);
+    // Initialize logging values
+    List<Pose3d> allTagPoses = new LinkedList<>();
+    List<Pose3d> allRobotPoses = new LinkedList<>();
+    List<Pose3d> allRobotPosesAccepted = new LinkedList<>();
+    List<Pose3d> allRobotPosesRejected = new LinkedList<>();
 
-      // TODO when testing start with no checks, then slowly add in order to not lose too much data
+    // Loop over cameras
+    for (int i = 0; i < io.length; i++) {
+      io[i].updateInputs(inputs[i], poseManager);
+      Logger.processInputs("AprilTagVision/Camera" + Integer.toString(i), inputs[i]);
 
-      Pose2d estimatedPose = inputs.estimatedPose.toPose2d();
-
+      // Initialize logging values
+      List<Pose3d> tagPoses = new LinkedList<>();
+      List<Pose3d> robotPoses = new LinkedList<>();
       List<Pose3d> robotPosesAccepted = new LinkedList<>();
       List<Pose3d> robotPosesRejected = new LinkedList<>();
 
+      Pose2d estimatedPose = inputs[i].estimatedPose.toPose2d();
+
       // Exit if there are no tags in sight or the pose is blank or the robot is spinning too fast
       // These are the basic checks LL recommends
-      double allowableDistance = inputs.tagCount;
+      double allowableDistance = inputs[i].tagCount;
       boolean isRejected =
           // true if
           // If no tags
-          inputs.tagCount == 0
+          inputs[i].tagCount == 0
               // if no pose, then ignored
               || estimatedPose.equals(new Pose2d())
               // if turning too fast
               || Math.abs(poseManager.robotVelocity().dtheta) > 720
           // if off the ground
-          // || inputs.estimatedPose.getY() > 0.15
+          // || inputs[i].estimatedPose.getY() > 0.15
           // if off field
           // || estimatedPose.getX() < -fieldBorderMargin
           // || estimatedPose.getX() > FieldConstants.fieldLength + fieldBorderMargin
@@ -63,18 +78,18 @@ public class AprilTagVision extends VirtualSubsystem {
           // || poseManager.getDistanceTo(estimatedPose) > allowableDistance
           ;
 
+      robotPoses.add(inputs[i].estimatedPose);
       if (isRejected) {
-        robotPosesRejected.add(inputs.estimatedPose);
-
+        robotPosesRejected.add(inputs[i].estimatedPose);
       } else {
-        robotPosesAccepted.add(inputs.estimatedPose);
+        robotPosesAccepted.add(inputs[i].estimatedPose);
       }
 
       // Smaller number = more trust
       double trust = .7;
 
       // Scale trust based on number of tags
-      // if (inputs.tagCount < 2) {
+      // if (inputs[i].tagCount < 2) {
       //   trust *= 2;
       // }
 
@@ -101,7 +116,7 @@ public class AprilTagVision extends VirtualSubsystem {
       Matrix<N3, N1> stdDevs = VecBuilder.fill(trust, trust, 99999);
 
       // Add result because all checks passed
-      poseManager.addVisionMeasurement(estimatedPose, inputs.timestamp, stdDevs);
+      poseManager.addVisionMeasurement(estimatedPose, inputs[i].timestamp, stdDevs);
 
       Logger.recordOutput(
           "Vision/" + this.io[i].getName() + "/RobotPosesAccepted",
