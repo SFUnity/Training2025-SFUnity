22d588e0a0396c2ee8fcb8d78ee1b4d64f3a9c3d
SFUnityTeamAccount <sfunityrobotics@gmail.com>
2025-02-17 11:19:28 -0800

    fix L2 scoring

--- src/main/java/frc/robot/RobotCommands.java
+++ src/main/java/frc/robot/RobotCommands.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.carriage.Carriage;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.elevator.ElevatorConstants;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.util.PoseManager;
 import java.util.Map;
@@ -47,9 +48,11 @@ public final class RobotCommands {
             elevator
                 .enableElevator()
                 .alongWith(
-                    waitUntil(elevator::pastL3Height)
+                    either(
+                            waitUntil(elevator::pastL3Height).andThen(carriage.backUpForL3()),
+                            none(),
+                            () -> elevator.goalHeightInches > ElevatorConstants.pastL3Height.get())
                         .andThen(
-                            carriage.backUpForL3(),
                             waitUntil(() -> atPose.getAsBoolean() && elevator.atDesiredHeight()),
                             carriage.placeCoral())));
   }
