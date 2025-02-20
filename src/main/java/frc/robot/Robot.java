ddd1ee4b251d1ab13cc59537d28d7b992565118b
SFUnityTeamAccount <sfunityrobotics@gmail.com>
2025-02-19 18:41:50 -0800

    format

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -385,7 +385,11 @@ public class Robot extends LoggedRobot {
             Commands.select(
                     Map.of(
                         LeftBranch,
-                        scoreCoral(elevator, carriage, poseManager, atGoal(drive)), // driver.rightTrigger()
+                        scoreCoral(
+                            elevator,
+                            carriage,
+                            poseManager,
+                            atGoal(drive)), // driver.rightTrigger()
                         Dealgify,
                         dealgify(elevator, carriage, poseManager),
                         ProcessorFront,
