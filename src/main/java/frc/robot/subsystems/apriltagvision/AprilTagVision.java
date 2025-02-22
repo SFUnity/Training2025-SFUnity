758fdafa9274fd59c9e9e22d6cf21fe49c00a768
SFUnityTeamAccount <sfunityrobotics@gmail.com>
2025-02-22 13:55:32 -0800

    add logging

--- src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVision.java
+++ src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVision.java
@@ -89,7 +89,9 @@ public class AprilTagVision extends VirtualSubsystem {
               || estimatedPose.getY() > FieldConstants.fieldWidth + fieldBorderMargin
               // if too far away from current pose, depends on amount of apriltags
               || (poseManager.getDistanceTo(estimatedPose) > allowableDistance
-                  && poseManager.getDistanceTo(new Pose2d()) > 0.2);
+                  && poseManager.getDistanceTo(new Pose2d()) > 0.75);
+
+      Logger.recordOutput("distanceToBlankPose", poseManager.getDistanceTo(new Pose2d()));
 
       robotPoses.add(inputs[i].estimatedPose);
       if (isRejected) {
