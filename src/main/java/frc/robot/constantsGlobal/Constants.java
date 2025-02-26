c567568e210b2faddcd3ac796c7a68805623a10f
MrTinker64 <avi.micah@gmail.com>
2025-02-26 10:09:08 -0800

    change sim mode back to sim bc that's what it should be

--- src/main/java/frc/robot/constantsGlobal/Constants.java
+++ src/main/java/frc/robot/constantsGlobal/Constants.java
@@ -26,7 +26,7 @@ import edu.wpi.first.wpilibj.RobotBase;
 public final class Constants {
   public static final double loopPeriodSecs = 0.02;
   public static final Mode currentMode =
-      RobotBase.isReal() ? Mode.REAL : Mode.REPLAY; // Switch Mode.SIM to Mode.REPLAY to run replay
+      RobotBase.isReal() ? Mode.REAL : Mode.SIM; // Switch Mode.SIM to Mode.REPLAY to run replay
   public static final boolean tuningMode = true;
 
   public static enum Mode {
