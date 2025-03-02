c49c691e45f5b0e4a0b52ed941171a18531b4cfd
MrTinker64 <avi.micah@gmail.com>
2025-03-02 08:02:06 -0800

    improve elevator zeroing

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -149,8 +149,8 @@ public class Elevator extends SubsystemBase {
 
   public Command runCurrentZeroing() {
     return run(() -> io.runVolts(-1.0))
-        .until(() -> inputs.currentAmps > 30.0)
-        .andThen(run(() -> io.runVolts(0)).withTimeout(0.5))
+        .until(() -> inputs.currentAmps > 40.0)
+        .andThen(run(() -> io.runVolts(0)).withTimeout(0.2))
         .finallyDo(() -> io.resetEncoder(0.0));
   }
 
