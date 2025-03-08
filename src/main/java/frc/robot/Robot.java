3cd4ab80a9afd08214fde829360a2d7c3b89dcfd
SFUnityTeamAccount <sfunityrobotics@gmail.com>
2025-03-08 12:03:34 -0800

    try fixing conflicting commands

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -35,6 +35,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.constantsGlobal.BuildConstants;
@@ -454,7 +455,7 @@ public class Robot extends LoggedRobot {
                     either(
                         either(
                                 drive
-                                    .fullAutoDrive(goalPose(poseManager))
+                                    .fullAutoDrive(goalPose(poseManager)).withInterruptBehavior(InterruptionBehavior.kCancelIncoming)
                                     .andThen(
                                         either(
                                             drive.driveIntoWall(),
