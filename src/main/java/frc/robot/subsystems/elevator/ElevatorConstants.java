a808ea463659c89876f12ce49348972af1d2e072
SFUnityTeamAccount <sfunityrobotics@gmail.com>
2025-03-08 12:55:00 -0800

    hardcode height

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -50,7 +50,7 @@ public class ElevatorConstants {
     L3(new LoggedTunableNumber("Elevator/L3", maxHeightInches)),
     L2(new LoggedTunableNumber("Elevator/L2", 8)),
     L1(new LoggedTunableNumber("Elevator/L1", 8)),
-    AlgaeHigh(new LoggedTunableNumber("Elevator/AlgaeHigh", 18)),
+    AlgaeHigh(new LoggedTunableNumber("Elevator/AlgaeHigh", 19)),
     AlgaeLow(new LoggedTunableNumber("Elevator/AlgaeLow", L3.get())),
     IceCream(new LoggedTunableNumber("Elevator/IceCream", 5)),
     Processor(new LoggedTunableNumber("Elevator/Processor", 2));
