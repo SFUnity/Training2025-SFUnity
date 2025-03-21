7609138996c22709216e20ec61b1968082162947
SFUnityTeamAccount <sfunityrobotics@gmail.com>
2025-03-20 17:53:50 -0700

    format

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -47,10 +47,13 @@ import frc.robot.subsystems.apriltagvision.AprilTagVisionIOLimelight;
 import frc.robot.subsystems.carriage.Carriage;
 import frc.robot.subsystems.carriage.CarriageIO;
 import frc.robot.subsystems.carriage.CarriageIOSim;
+import frc.robot.subsystems.carriage.CarriageIOSparkMax;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.DriveConstants.DriveCommandsConfig;
 import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIOPigeon2;
 import frc.robot.subsystems.drive.ModuleIO;
+import frc.robot.subsystems.drive.ModuleIOMixed;
 import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.elevator.ElevatorIO;
@@ -208,21 +211,21 @@ public class Robot extends LoggedRobot {
         // Real robot, instantiate hardware IO implementations
         drive =
             new Drive(
-                // new GyroIOPigeon2(),
-                // new ModuleIOMixed(0),
-                // new ModuleIOMixed(1),
-                // new ModuleIOMixed(2),
-                // new ModuleIOMixed(3),
-                new GyroIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
+                new GyroIOPigeon2(),
+                new ModuleIOMixed(0),
+                new ModuleIOMixed(1),
+                new ModuleIOMixed(2),
+                new ModuleIOMixed(3),
+                // new GyroIO() {},
+                // new ModuleIO() {},
+                // new ModuleIO() {},
+                // new ModuleIO() {},
+                // new ModuleIO() {},
                 poseManager,
                 driveCommandsConfig);
         elevator = new Elevator(new ElevatorIOSparkMax(), poseManager);
-        carriage = new Carriage(new CarriageIO() {});
-        intake = new Intake(new IntakeIO(){});
+        carriage = new Carriage(new CarriageIOSparkMax());
+        intake = new Intake(new IntakeIOSparkMax());
         vision =
             new AprilTagVision(
                 poseManager,
