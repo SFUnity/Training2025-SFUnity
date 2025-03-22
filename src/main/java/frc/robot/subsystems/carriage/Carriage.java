ac4cb1c7ddcae9617e1dd0f3df2566fb682abeb4
MrTinker64 <avi.micah@gmail.com>
2025-03-22 15:10:58 -0700

    make full coral held boolean

--- src/main/java/frc/robot/subsystems/carriage/Carriage.java
+++ src/main/java/frc/robot/subsystems/carriage/Carriage.java
@@ -31,10 +31,14 @@ public class Carriage extends SubsystemBase {
   private boolean coralPassed = false;
   private boolean realCoralHeld = false;
   private boolean realAlgaeHeld = false; 
+  private boolean fullCoralHeld = false;
 
   private final Timer beambreakTimer = new Timer();
   private static final LoggedTunableNumber beambreakDelay =
       new LoggedTunableNumber("Carriage/beambreakDelay", 0.35);
+  private final Timer fullCoralHeldTimer = new Timer();
+  private static final LoggedTunableNumber fullCoralHeldDelay =
+      new LoggedTunableNumber("Carriage/fullCoralHeldDelay", 0.35);
 
   private static final LoggedTunableNumber backupForL3Rots =
       new LoggedTunableNumber("Carriage/Backup for L3 Rots", 15);
@@ -97,6 +101,14 @@ public class Carriage extends SubsystemBase {
     }
     if (!beamBreak() || coralPassed || realCoralHeld) {
       beambreakTimer.restart();
+    }
+    if (beamBreak() && realCoralHeld) {
+      if (fullCoralHeldTimer.hasElapsed(fullCoralHeldDelay.get())) {
+        fullCoralHeld = true;
+      }
+    } else {
+      fullCoralHeld = false;
+      fullCoralHeldTimer.restart();
     }
   }
 
