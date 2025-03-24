4c8228d1f8b975e0c1600976141aa08a7f07fdbd
MINAJECHUNG <mchung27@urbanschool.org>
2025-03-24 13:40:24 -0700

    create funnelIO

--- src/main/java/frc/robot/subsystems/funnel/FunnelIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelIOSparkMax.java
@@ -1,5 +1,15 @@
 package frc.robot.subsystems.funnel;
 
 public class FunnelIOSparkMax {
-
+    @AutoLog
+    public static class FunnelIOInputs {
+      public double position = 0.0;
+      public double velocityInchesPerSec = 0.0;
+      public double appliedVolts = 0.0;
+      public double currentAmps = 0.0;
+    }
+  
+    public default void updateInputs(ElevatorIOInputs inputs) {}
+  
+    public default void runVolts(double volts) {}
 }
