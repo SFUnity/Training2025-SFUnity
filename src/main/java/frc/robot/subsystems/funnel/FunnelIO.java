162e4f83dd5189eb897b1f62ab1bf1586232bfcf
MINAJECHUNG <mchung27@urbanschool.org>
2025-03-24 13:41:42 -0700

    actually put IO in right file skull

--- src/main/java/frc/robot/subsystems/funnel/FunnelIO.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelIO.java
@@ -1,5 +1,13 @@
 package frc.robot.subsystems.funnel;
 
 public interface FunnelIO {
-
+    @AutoLog
+    public static class FunnelIOInputs {
+      public double appliedVolts = 0.0;
+      public double currentAmps = 0.0;
+    }
+  
+    public default void updateInputs(ElevatorIOInputs inputs) {}
+  
+    public default void runVolts(double volts) {}
 }
