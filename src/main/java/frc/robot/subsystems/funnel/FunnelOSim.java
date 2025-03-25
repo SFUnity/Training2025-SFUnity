379bc1b8f380c026f6bbadf0acb0146ffd828074
MINAJECHUNG <mchung27@urbanschool.org>
2025-03-25 08:32:46 -0700

    create sim class

--- src/main/java/frc/robot/subsystems/funnel/FunnelOSim.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelOSim.java
@@ -1,3 +1,19 @@
 package frc.robot.subsystems.funnel;
 
-public class FunnelOSim {}
+import frc.robot.subsystems.carriage.CarriageIO.CarrageIOInputs;
+
+public class FunnelOSim implements FunnelIO{
+    private double appliedVolts = 0.0;
+
+  public FunnelOSim() {}
+
+  @Override
+  public void updateInputs(FunnelIOInputs inputs) {
+    inputs.appliedVolts = appliedVolts;
+  }
+
+  @Override
+  public void runVolts(double volts) {
+    appliedVolts = volts;
+  }
+}
