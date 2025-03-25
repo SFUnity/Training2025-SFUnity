ad06e2900160043151d77d541191b7940aa38c95
MINAJECHUNG <mchung27@urbanschool.org>
2025-03-25 08:41:47 -0700

    create funnel class

--- src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
@@ -1,7 +1,14 @@
 package frc.robot.subsystems.funnel;
 
+import frc.robot.util.LoggedTunableNumber;
+
 public class FunnelConstants {
   public static final int funnelMotorID = 0;
   public static final boolean inverted = false;
   public static final int positionFactor = 5;
+
+  public static final LoggedTunableNumber rollerSpeedVolts = new LoggedTunableNumber("Funnel/rollerSpeed", 0);
 }
+
+  
+
