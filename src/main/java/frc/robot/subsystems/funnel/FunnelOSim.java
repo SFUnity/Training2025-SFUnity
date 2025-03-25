ee046a7a056a7714e27fa383a4241e95b99d0b7d
MINAJECHUNG <mchung27@urbanschool.org>
2025-03-25 08:33:00 -0700

    format

--- src/main/java/frc/robot/subsystems/funnel/FunnelOSim.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelOSim.java
@@ -1,9 +1,7 @@
 package frc.robot.subsystems.funnel;
 
-import frc.robot.subsystems.carriage.CarriageIO.CarrageIOInputs;
-
-public class FunnelOSim implements FunnelIO{
-    private double appliedVolts = 0.0;
+public class FunnelOSim implements FunnelIO {
+  private double appliedVolts = 0.0;
 
   public FunnelOSim() {}
 
