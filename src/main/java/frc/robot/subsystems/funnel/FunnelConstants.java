5d398267fcbf3a863a2c56809156c64f242bba01
MrTinker64 <avi.micah@gmail.com>
2025-03-28 08:30:15 -0700

    update position factor

--- src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelConstants.java
@@ -5,7 +5,7 @@ import frc.robot.util.LoggedTunableNumber;
 public class FunnelConstants {
   public static final int funnelMotorID = 0;
   public static final boolean inverted = false;
-  public static final int positionFactor = 5;
+  public static final int positionFactor = 3;
 
   public static final LoggedTunableNumber rollerSpeedVolts =
       new LoggedTunableNumber("Funnel/rollerSpeed", 2);
