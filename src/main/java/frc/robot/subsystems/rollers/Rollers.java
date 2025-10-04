44a79e0066d08fcf1d3a157ee280c91da5c6cfa3
MrTinker64 <avi.micah@gmail.com>
2025-10-04 15:44:48 -0700

    set up rollers subsystem

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -3,5 +3,11 @@ package frc.robot.subsystems.rollers;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Rollers extends SubsystemBase {
+  private final RollersIO io;
+  private final RollersIOInputsAutoLogged inputs = new RollersIOInputsAutoLogged();
+
+  public Rollers(RollersIO io) {
+    this.io = io;
+  }
     
 }
