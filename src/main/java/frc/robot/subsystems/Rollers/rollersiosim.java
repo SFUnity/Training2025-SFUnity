333491e9eadfc1e6ba566cd14f03c03ffc3f795d
MInoru412 <esatow29@urbanschool.org>
2025-10-04 15:24:46 -0700

    added overides

--- src/main/java/frc/robot/subsystems/Rollers/rollersiosim.java
+++ src/main/java/frc/robot/subsystems/Rollers/rollersiosim.java
@@ -1,5 +1,19 @@
 package frc.robot.subsystems.Rollers;
 
-public class rollersiosim {
-    
+import com.google.errorprone.annotations.OverridingMethodsMustInvokeSuper;
+
+public class rollersiosim implements rollersio {
+    private double appliedVolts = 0.0;
+
+    public rollersiosim() {}
+
+    @Override
+    public void updateInputs (RollersioInputs inputs) {
+        inputs.voltage = appliedVolts;
+    }
+
+    @Override
+    public void runVolts (double volts) {
+        appliedVolts = volts;
+    }
 }
