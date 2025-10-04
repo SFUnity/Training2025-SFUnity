7585bdec84447cb50d76a6f564bb0c4ff8ef8a94
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2025-10-04 15:24:53 -0700

    add rolleriosim, fixed other rollersio

--- src/main/java/frc/robot/subsystems/rollers/RollersIO.java
+++ src/main/java/frc/robot/subsystems/rollers/RollersIO.java
@@ -11,11 +11,8 @@ public interface RollersIO {
         public double positionRad = 0.0;
     }
 
-    public default void updateInputs(RollersIOInputs inputs) {
-
-    }
+    public default void updateInputs(RollersIOInputs inputs) {}
 
     public default void runVolts(double volts) {}
-
-    // public 
+    
 }
