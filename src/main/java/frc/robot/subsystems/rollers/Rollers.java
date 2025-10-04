0ecfafec353b7bc8cab632fa12fd125a32880cd2
Vihaan Paka-Hegde <vihaan.ph@gmail.com>
2025-10-04 15:45:28 -0700

    Set up rollers subsystem

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems.rollers;
 
-public class Rollers {
-    
-}
+public class Rollers 
+
+public class Rollers extends SubsystemBase {
+    private final RollersIO io;
+    private final RollersIO.RollersIOIinputs inputs = new RollersIO.RollersIOIinputs();
+
+    public Rollers(RollersIO io) {
+        this.io = io;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+    }
+
+    public Command command() {
+        return run(() -> io.runVolts());
+    }
+    }
+
