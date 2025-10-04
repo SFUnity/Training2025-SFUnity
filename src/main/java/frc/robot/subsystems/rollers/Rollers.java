e511bc2cc5dbcbd456002d6edf85236ea6ed94a9
LILLIAN ORMES <lillian.ormes_28@sfuhs.org>
2025-10-04 15:52:30 -0700

    format rollers to Sim

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -1,7 +1,26 @@
 package frc.robot.subsystems.rollers;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Rollers extends SubsystemBase{
-    
+public class Rollers extends SubsystemBase {
+  private final RollersIO io;
+  private final RollersIOInputsAutoLogged inputs = new RollersIOInputsAutoLogged();
+
+  public Rollers(RollersIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+  }
+
+  public Command intake() {
+    return run(() -> io.runVolts(10));
+  }
+
+  public Command stop() {
+    return run(() -> io.runVolts(0.0));
+  }
 }
