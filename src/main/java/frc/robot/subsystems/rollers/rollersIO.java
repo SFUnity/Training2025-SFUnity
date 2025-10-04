429aca3d2645457abe5b697add32a9ce593ff9ee
Emma <emma.posey_29@gmail.com>
2025-10-04 15:13:02 -0700

    add basic methods

--- src/main/java/frc/robot/subsystems/rollers/rollersIO.java
+++ src/main/java/frc/robot/subsystems/rollers/rollersIO.java
@@ -1,5 +1,18 @@
 package frc.robot.subsystems.rollers;
 
-public class rollersIO {
-    
+import org.littletonrobotics.junction.AutoLog;
+
+public interface rollersIO {
+
+    @AutoLog
+    public static class RollerIOInputs {   
+        public double current = 0.0;
+        public double voltage = 0.0;
+        public double positionRad = 0.0;
+    }
+
+    public default void updateInputs {RollersIOInputs inputs} {}
+
+    public default void runVolts{double volts} {}
 }
+
