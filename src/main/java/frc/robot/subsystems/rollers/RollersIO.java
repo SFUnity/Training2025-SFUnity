a5bda5bea2cf80e2269acd932f163e71d25acad0
MrTinker64 <avi.micah@gmail.com>
2025-10-04 15:12:33 -0700

    Add basic methods

--- src/main/java/frc/robot/subsystems/rollers/RollersIO.java
+++ src/main/java/frc/robot/subsystems/rollers/RollersIO.java
@@ -10,4 +10,8 @@ public interface RollersIO {
         public double voltage = 0.0;
         public double positionRad = 0.0;
     }
+
+    public default void updateInputs(RollersIOInputs inputs) {}
+
+    public default void runVolts(double volts) {}
 }
