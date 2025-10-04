69c00dd69f5cb5db970de8be960732df58f81000
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2025-10-04 15:47:42 -0700

    add rollers class

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -1,5 +1,26 @@
 package frc.robot.subsystems.rollers;
 
-public class Rollers {
-    
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Rollers extends SubsystemBase {
+  private final RollersIO io;
+  private final RollersIOInputsAutoLogged inputs = new RollersIOInputsAutoLogged();
+
+  public Rollers(RollersIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+  }
+
+  public Command intake() {
+    return run(() -> io.runVolts(6.0));
+  }
+
+  public Command stop() {
+    return run(() -> io.runVolts(0.0));
+  }
 }
