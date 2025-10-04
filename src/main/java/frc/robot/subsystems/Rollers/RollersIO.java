6924a45925b6b33dd6b5941bc80cbf8baa9bdc94
Mishi Doron <mishi.doron@gmail.com>
2025-10-04 15:13:12 -0700

    add baisc methods

--- src/main/java/frc/robot/subsystems/Rollers/RollersIO.java
+++ src/main/java/frc/robot/subsystems/Rollers/RollersIO.java
@@ -10,4 +10,10 @@ public interface RollersIO {
         public double voltage = 0.0;        
         public double positionRad = 0.0;
     }
+
+    public default void UpdateInputs(RollersIOInputs inputs) {}
+
+    public default void runVolts(double volts) {
+        
+    }
 }
