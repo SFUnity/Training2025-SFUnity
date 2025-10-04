fd72c30a66773963bee0de1b16bb93f8a02cd3d7
Ian Lee <ileef@Ians-MacBook-Air-2.local>
2025-10-04 15:24:30 -0700

    add basic variables and methods of rollersIO sim

--- src/main/java/frc/robot/subsystems/rollers/RollersIOSim.java
+++ src/main/java/frc/robot/subsystems/rollers/RollersIOSim.java
@@ -1,5 +1,12 @@
 package frc.robot.subsystems.rollers;
 
 public class RollersIOSim implements RollersIO{
+    private double appliedVolts = 0.0;
+
+    public RollersIOSim() {}
     
+    @Override
+    public void updateInputs(RollersIOInputs inputs) {
+        inputs.voltage = appliedVolts;
+    }
 }
