67098bad6884c1e3fa8e1aa5d075246d54685bda
MINAJECHUNG <mchung27@urbanschool.org>
2025-10-06 11:23:39 -0700

    add update inputs for sim

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,9 +1,30 @@
 package frc.robot.subsystems.intake;
 
 import edu.wpi.first.math.util.Units;
+import frc.robot.constantsGlobal.Constants;
+import frc.robot.util.LoggedTunableNumber;
 
 public class IntakeConstants {
   public static final double minAngleRads = Units.degreesToRadians(10);
   public static final double maxAngleRads = Units.degreesToRadians(135);
   public static final double armLengthMeters = Units.inchesToMeters(15);
+  public static final LoggedTunableNumber kP;
+
+
+  static {
+    updateTunables();
+
+    switch (Constants.currentMode) {
+      default:
+        kP = new LoggedTunableNumber("Intake/kP", 0.028);
+        break;
+      case SIM:
+        kP = new LoggedTunableNumber("Intake/simkP", 0.1);
+        break;
+    }
+  }
+
+
+  public static void updateTunables() {
+  }
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.intake;
 
 import static frc.robot.subsystems.intake.IntakeConstants.*;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
@@ -16,4 +17,21 @@ public class IntakeIOSim implements IntakeIO {
           maxAngleRads,
           false,
           minAngleRads);
+    private final PIDController pid;
+    private double pivotVolts = 0.0;
+    private double rollersVolts = 0.0;
+
+    public IntakeIOSim(){
+        pid = new PIDController(kP.get(), 0.0, 0.0);
+        sim.setState(minAngleRads, 0.0);
+    }
+
+    @Override
+    public void updateInputs(IntakeIOInputs inputs){
+        sim.update(0.02);
+        inputs.pivotPositionRads = sim.getAngleRads() - minAngleRads;
+        inputs.pivotVoltage = pivotVolts;
+        inputs.pivotCurrent = sim.getCurrentDrawAmps();
+        inputs.rollerVoltage = rollersVolts;
+    }  
 }
