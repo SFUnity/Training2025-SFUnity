cb3f2d6a54b70df10974e77d99b51a4c8c143a3c
MINAJECHUNG <mchung27@urbanschool.org>
2025-10-06 10:46:21 -0700

    intake interface

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems.intake;
+
+public interface IntakeIO {
+    @AutoLog
+    public static class IntakeIOInputs {
+        public double pivotCurrent = 0.0;
+        public double pivotVoltage = 0.0;
+        public double pivotPositionRads = 0.0;
+
+        public double rollerCurrent = 0.0;
+        public double rollerVoltage = 0.0;
+    }
+    
+    default void updateInputs(IntakeIOInputs inputs) {}
+
+  default void runRollers(double volts) {}
+
+  default void runPivot(double volts) {}
+
+  default void setPivotPosition(double setpointDeg) {}
+
+  default void resetEncoder(double position) {}
+
+}
