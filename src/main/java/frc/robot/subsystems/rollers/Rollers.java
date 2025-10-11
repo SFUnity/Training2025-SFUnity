088828fa3889386115ae92023742ef3ba6b97b18
Vihaan Paka-Hegde <vihaan.ph@gmail.com>
2025-10-11 14:12:32 -0700

    Adjusted code for rollers

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -4,23 +4,27 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Rollers extends SubsystemBase {
-    private final RollersIO io;
-    private final RollersIO.RollersIOIinputs inputs = new RollersIO.RollersIOIinputs();
+  private final RollersIO io;
+  private final RollersIOInputsAutoLogged inputs = new RollersIOInputsAutoLogged();
 
-    public Rollers(RollersIO io) {
-        this.io = io;
-    }
+  public Rollers(RollersIO io) {
+    this.io = io;
+  }
 
-    @Override
-    public void periodic() {
-        io.updateInputs(inputs);
-    }
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+  }
 
-    public Command command() {
-        return run(() -> io.runVolts(0));
-    }
-    public Command eject() {
-        return run(() -> io.runVolts(-6.7));
-    }
-    }
+  public Command intake() {
+    return run(() -> io.runVolts(6.0));
+  }
 
+  public Command eject() {
+    return run(() -> io.runVolts(-6.0));
+  }
+
+  public Command stop() {
+    return run(() -> io.runVolts(0.0));
+  }
+}
