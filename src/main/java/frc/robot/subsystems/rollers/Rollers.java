5a34227a54a8f9bf03d1a4e213612d3862296cbf
MrTinker64 <avi.micah@gmail.com>
2025-10-12 13:04:38 -0700

    format

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -1,9 +1,8 @@
 package frc.robot.subsystems.rollers;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 
 public class Rollers extends SubsystemBase {
   private final RollersIO io;
