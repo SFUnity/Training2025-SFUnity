0bc13919b98966a472a6aba4b20816b60d1334ef
Gabkwo <kwongabriel2010@gmail.com>
2025-10-12 10:28:22 -0700

    add rollers tag

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -205,7 +205,7 @@ public class Robot extends LoggedRobot {
                 poseManager,
                 new AprilTagVisionIOLimelight(leftName),
                 new AprilTagVisionIOLimelight(rightName));
-        rollers = new Rollers(new RollersIOSim());
+        rollers = new Rollers(new RollersIOSim() {});
         break;
 
       case SIM:
@@ -221,6 +221,7 @@ public class Robot extends LoggedRobot {
                 driveCommandsConfig);
         vision =
             new AprilTagVision(poseManager, new AprilTagVisionIO() {}, new AprilTagVisionIO() {});
+            rollers = new Rollers(new RollersIOSim() {});
         break;
 
       default:
