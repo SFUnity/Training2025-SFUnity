121322e32506e83c1dec330512171474ac0df26f
Gabkwo <kwongabriel2010@gmail.com>
2025-10-12 10:32:28 -0700

    add Logger.processInputs

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.rollers;
 
+import org.littletonrobotics.junction.Logger;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -13,6 +14,7 @@ public class Rollers extends SubsystemBase {
 
   @Override
   public void periodic() {
+    Logger.processInputs("Rollers", inputs);
     io.updateInputs(inputs);
   }
 
