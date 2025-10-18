36db2771f276d0e12b4d5fbd7ddc63c862c5680a
MrTinker64 <avi.micah@gmail.com>
2025-10-18 15:02:48 -0700

    more formatting

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -95,12 +95,7 @@ public class Autos {
     AutoTrajectory driveToMiddle = routine.trajectory("Starting Path");
 
     // When the routine begins, reset odometry and start the first trajectory (1)
-    routine.active().onTrue(
-        Commands.sequence(
-            driveToMiddle.resetOdometry(),
-            driveToMiddle.cmd()
-        )
-    );
+    routine.active().onTrue(Commands.sequence(driveToMiddle.resetOdometry(), driveToMiddle.cmd()));
 
     return routine;
   }
