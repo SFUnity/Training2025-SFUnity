af7e414982bbfe74e036096e40d994b30ef07228
Angelina Liu <liu.angelinaca@gmail.com>
2025-10-18 14:45:51 -0700

    add routine to chooser

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -57,6 +57,7 @@ public class Autos {
 
     /* Set up main choreo routines */
     chooser = new LoggedAutoChooser("ChoreoChooser");
+    chooser.addRoutine("Example Auto Routine", this::pickupAndScoreAuto);
     // chooser.addRoutine("Example Auto Routine", this::exampleAutoRoutine);
 
     if (!DriverStation.isFMSAttached()) {
