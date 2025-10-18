4e6ab6a9c4258542c88a76c886ab2894332159d0
LILLIAN ORMES <lillian.ormes_28@sfuhs.org>
2025-10-18 15:43:36 -0700

    add rollers eject to auto routine

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -94,7 +94,9 @@ public class Autos {
 
     // When the routine begins, reset odometry and start the first trajectory (1)
     routine.active().onTrue(Commands.sequence(driveToMiddle.resetOdometry(),driveToMiddle.cmd()));
-
+    
+    routine.trajectory("Start Path").done().onTrue(rollers.eject().withTimeout(1));
+    
     return routine;
   }
 }
