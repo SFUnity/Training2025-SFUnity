7642e1600ea09ee774274953b163270ac15e4a0b
LILLIAN ORMES <lillian.ormes_28@sfuhs.org>
2025-10-18 14:45:50 -0700

    add routine to chooser

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -57,7 +57,7 @@ public class Autos {
 
     /* Set up main choreo routines */
     chooser = new LoggedAutoChooser("ChoreoChooser");
-    // chooser.addRoutine("Example Auto Routine", this::exampleAutoRoutine);
+    chooser.addRoutine("Example Auto Routine", this::pickupAndScoreAuto);
 
     if (!DriverStation.isFMSAttached()) {
       // Set up test choreo routines
@@ -86,7 +86,7 @@ public class Autos {
   public Command getAutonomousCommand() {
     return isChoreoAuto ? chooser.selectedCommandScheduler() : nonChoreoChooser.get();
   }
-  public AutoRoutine pickupAndScoreAuto() {
+  private AutoRoutine pickupAndScoreAuto() {
     AutoRoutine routine = factory.newRoutine("taxi");
 
     // Load the routine's trajectories
