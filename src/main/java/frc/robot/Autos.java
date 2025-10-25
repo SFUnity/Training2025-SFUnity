e7c2c5b8d663c260d69bb6aca2690e1801926a4b
LILLIAN ORMES <lillian.ormes_28@sfuhs.org>
2025-10-24 17:07:47 -0700

    add new routine name

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -58,7 +58,7 @@ public class Autos {
     /* Set up main choreo routines */
     chooser = new LoggedAutoChooser("ChoreoChooser");
     chooser.addRoutine("Example Auto Routine", this::pickupAndScoreAuto);
-    chooser.addRoutine("Feeder Intake Routine", this::intakeAndEjectAuto);
+    chooser.addRoutine("Intake and Eject Routine", this::intakeAndEjectAuto);
 
     if (!DriverStation.isFMSAttached()) {
       // Set up test choreo routines
