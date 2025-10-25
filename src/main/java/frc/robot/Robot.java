17475fac04c0d1aa6a339110ec0412e9c8a2f949
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2025-10-25 14:09:31 -0700

    fixed file names

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -221,7 +221,7 @@ public class Robot extends LoggedRobot {
                 driveCommandsConfig);
         vision =
             new AprilTagVision(poseManager, new AprilTagVisionIO() {}, new AprilTagVisionIO() {});
-            rollers = new Rollers(new RollersIOSim());
+        rollers = new Rollers(new RollersIOSim());
         break;
 
       default:
@@ -250,7 +250,7 @@ public class Robot extends LoggedRobot {
                     return rightName;
                   }
                 });
-                rollers = new Rollers(new RollersIO() {});
+        rollers = new Rollers(new RollersIO() {});
         break;
     }
 
