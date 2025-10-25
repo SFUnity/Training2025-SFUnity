9c5e2c84722bfeb3d2542440785ece1bfcd29a0f
Emma <emma.posey_29@gmail.com>
2025-10-25 13:58:40 -0700

    update rollers

--- src/main/java/frc/robot/subsystems/rollers/rollersIO.java
+++ src/main/java/frc/robot/subsystems/rollers/rollersIO.java
@@ -2,17 +2,16 @@ package frc.robot.subsystems.rollers;
 
 import org.littletonrobotics.junction.AutoLog;
 
-public interface rollersIO {
+public interface RollersIO {
 
-    @AutoLog
-    public static class RollerIOInputs {   
-        public double current = 0.0;
-        public double voltage = 0.0;
-        public double positionRad = 0.0;
-    }
+  @AutoLog
+  public static class RollersIOInputs {
+    public double current = 0.0;
+    public double voltage = 0.0;
+    public double positionRad = 0.0;
+  }
 
-    public default void updateInputs {RollersIOInputs inputs} {}
+  public default void updateInputs(RollersIOInputs inputs) {}
 
-    public default void runVolts{double volts} {}
+  public default void runVolts(double volts) {}
 }
-
