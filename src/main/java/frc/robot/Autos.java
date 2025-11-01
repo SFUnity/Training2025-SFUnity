37aa4641e1019efaad6d1eb9c9c664864a0e5c44
Ishaan Shrimali <ishaan1003@gmail.com>
2025-11-01 16:03:46 -0700

    Changed name of routine and added to it choreo chooser

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -57,7 +57,7 @@ public class Autos {
 
     /* Set up main choreo routines */
     chooser = new LoggedAutoChooser("ChoreoChooser");
-    // chooser.addRoutine("Example Auto Routine", this::exampleAutoRoutine);
+    chooser.addRoutine("Drive to Algae", this::driveToAlgae);
 
     if (!DriverStation.isFMSAttached()) {
       // Set up test choreo routines
