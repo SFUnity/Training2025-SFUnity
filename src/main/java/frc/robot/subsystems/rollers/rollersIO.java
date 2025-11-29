f66dce6dfc757c3ac1e362a9fa3edfa7edb5815b
Ishaan Shrimali <ishaan1003@gmail.com>
2025-11-29 15:55:27 -0800

    Renamed Voltage to Volts

--- src/main/java/frc/robot/subsystems/rollers/rollersIO.java
+++ src/main/java/frc/robot/subsystems/rollers/rollersIO.java
@@ -5,10 +5,10 @@ import org.littletonrobotics.junction.AutoLog;
 public interface RollersIO {
   @AutoLog
   public static class RollersIOInputs {
-    public double voltage = 0.0;
+    public double volts = 0.0;
   }
 
   public default void updateInputs(RollersIOInputs inputs) {}
 
-  public default void runVolts(double voltage) {}
+  public default void runVolts(double volts) {}
 }
--- src/main/java/frc/robot/subsystems/rollers/rollersIOSIM.java
+++ src/main/java/frc/robot/subsystems/rollers/rollersIOSIM.java
@@ -7,7 +7,7 @@ public class RollersIOSim implements RollersIO {
 
   @Override
   public void updateInputs(RollersIOInputs inputs) {
-    inputs.voltage = appliedVolts;
+    inputs.volts = appliedVolts;
   }
 
   @Override
