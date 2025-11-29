6fd906ef9b48f05bee5ba37af64cbcc310357634
Ishaan Shrimali <ishaan1003@gmail.com>
2025-11-29 15:38:51 -0800

    Debugged RollersIO

--- src/main/java/frc/robot/subsystems/rollers/rollersIO.java
+++ src/main/java/frc/robot/subsystems/rollers/rollersIO.java
@@ -3,12 +3,12 @@ package frc.robot.subsystems.rollers;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface RollersIO {
-    @AutoLog
-    public static class RollersIOInputs {
-        public double example_input = 0.0;
-    }
-    
-    public default void updateInputs(RollersIOInputs inputs){}
+  @AutoLog
+  public static class RollersIOInputs {
+    public double example_input = 0.0;
+  }
 
-    public default void exampleMethod(double example_input) {}
+  public default void updateInputs(RollersIOInputs inputs) {}
+
+  public default void exampleMethod(double example_input) {}
 }
