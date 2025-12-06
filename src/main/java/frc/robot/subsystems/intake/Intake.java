6287a5e58c16dcee368eeb959d54c1f088e7c8bf
Ishaan Shrimali <ishaan1003@gmail.com>
2025-12-06 15:21:51 -0800

    Turned into comments so we can buiild code

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -10,29 +10,29 @@ public class Intake extends SubsystemBase {
   // private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
 
   public Intake(IntakeIO io) {
-    this.io = io;
+    //this.io = io;
   }
 
   @Override
   public void periodic() {
-    io.updateInputs(inputs);
-    Logger.processInputs("Intake/Inputs", inputs);
+    //io.updateInputs(inputs);
+    //Logger.processInputs("Intake/Inputs", inputs);
   }
 
-  public Command intake() {
-    return run(() -> io.runVolts(6.0));
+  //public Command intake() {
+    //return run(() -> io.runVolts(6.0));
   }
 
-  public Command eject() {
-    return run(() -> io.runVolts(-6.0));
-  }
+  //public Command eject() {
+    //return run(() -> io.runVolts(-6.0));
+  //}
 
-  public Command doStuff() {
-    return new SequentialCommandGroup(
-        intake().withTimeout(3), stop().withTimeout(1), eject().withTimeout(2));
-  }
+  //public Command doStuff() {
+    //return new SequentialCommandGroup(
+        //intake().withTimeout(3), stop().withTimeout(1), eject().withTimeout(2));
+  //}
 
-  public Command stop() {
-    return run(() -> io.runVolts(0.0));
-  }
-}
+  //public Command stop() {
+    //return run(() -> io.runVolts(0.0));
+  //}
+//}
