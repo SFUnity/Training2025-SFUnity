b56b0e384e4403679ba8b484c9f2b6371a0d65c1
Ishaan Shrimali <ishaan1003@gmail.com>
2025-12-06 13:54:57 -0800

    Created first sequentialcommand

--- src/main/java/frc/robot/subsystems/rollers/Rollers.java
+++ src/main/java/frc/robot/subsystems/rollers/Rollers.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.rollers;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
@@ -25,8 +26,16 @@ public class Rollers extends SubsystemBase {
   public Command eject() {
     return run(() -> io.runVolts(-6.0));
   }
+  
+  public Command doStuff() {
+    return new SequentialCommandGroup(
+        intake().withTimeout(3),
+        stop().withTimeout(1),
+        eject().withTimeout(2) 
+    );
+  }
 
   public Command stop() {
-    return run(() -> io.runVolts(0.0));
+    return run(() -> io.runVolts(0.0));   
   }
 }
