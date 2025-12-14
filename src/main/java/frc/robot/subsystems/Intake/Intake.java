22b3b6d700730bb9737456f81ae19e4eff6945b3
Mishi Doron <mishi.doron@gmail.com>
2025-12-14 13:41:47 -0800

    Add IntakeIO.java

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,5 +1,13 @@
 package frc.robot.subsystems.Intake;
 
-public class Intake {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+    private IntakeIO io;
+
+    public Intake(IntakeIO io) {
+        this.io = io;
+    }
 
 }
--- src/main/java/frc/robot/subsystems/Intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIO.java
@@ -1,5 +1,19 @@
 package frc.robot.subsystems.Intake;
 
-public class IntakeIO {
+import org.littletonrobotics.junction.AutoLog;
 
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
+public interface IntakeIO {
+    
+    @AutoLog
+    public static class RollersIOInputs {
+        public double current = 0.0;
+        public double voltage = 0.0;
+        public double positionRad = 0.0;
+    }
+
+    public default void updateInputs(RollersIOInputs inputs) {}
+
+    public default void runVolts(RollersIOInputs inputs) {}
 }
